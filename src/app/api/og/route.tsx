cb7beea42b1faef34f92ac3fe0e14067cf29a691
Aziz Khasyi <azizkhasyi11@gmail.com>
2024-10-29 22:44:36 +0700

    refactor: comment out dynamic response setting in Open Graph API route

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -3,7 +3,7 @@ import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
 
 // export const runtime = "edge"; // Use Edge runtime for faster response times
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
